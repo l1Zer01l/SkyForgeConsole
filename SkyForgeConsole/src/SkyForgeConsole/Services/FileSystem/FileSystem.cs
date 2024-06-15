6fc1583eaec0a99d601d0fa2a9e59723ed3d7a68
Hard_Code <zero-0_0@bk.ru>
2024-06-15 14:14:33 +0500

    Added FileSystem

---
+++ SkyForgeConsole/src/SkyForgeConsole/Services/FileSystem/FileSystem.cs
@@ -0,0 +1,91 @@
+﻿/**************************************************************************\
+    Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using SkyForgeConsole.Services.LogSystem;
+
+namespace SkyForgeConsole.Services
+{
+    public static class FileSystem
+    {
+        public static void CreateDirectory(string pathDirectory)
+        {
+            Directory.CreateDirectory(pathDirectory);
+        }
+        public static bool IsHaveDirectory(string pathDirectory)
+        {
+            return Directory.Exists(pathDirectory);
+        }
+        public static bool IsHaveFile(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+        public static string GetCurrentDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+        public static string GetFullPath(string directoryName)
+        {
+            return Path.GetFullPath(directoryName);
+        }
+        public static string CombinePath(string path1, string path2)
+        {
+            return Path.Combine(path1, path2);
+        }
+
+        public static void WriteToFile(string massage, string filePath)
+        {
+
+#if UNITTEST
+            return;
+#endif
+            try
+            {
+                using (var textWriter = File.AppendText(filePath))
+                {
+                    textWriter.WriteLine(massage);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.CoreLogger?.Logging($"Error can't write to file: {filePath}. Exception {ex}", LogLevel.Error);
+            }
+        }
+        public static List<string>? ReadFromFile(string filePath)
+        {
+
+#if UNITTEST
+            return null;
+#endif
+            return ReadFromFile(filePath, 0, 0);
+        }
+        public static List<string>? ReadFromFile(string filePath, int removeStart, int removeEnd)
+        {
+
+#if UNITTEST
+            return null;
+#endif
+
+            if (!IsHaveFile(filePath))
+            {
+                Log.CoreLogger?.Logging($"Error can't read file: {filePath}. ", LogLevel.Error);
+                return null;
+            }
+
+            using (var textReader = File.OpenText(filePath))
+            {
+                var result = new List<string>();
+                var textLine = textReader.ReadLine();
+                while (textLine != null)
+                {
+                    result.Add(textLine.Remove(removeStart, removeEnd));
+                    textLine = textReader.ReadLine();
+                }
+                textReader.Close();
+                textReader.Dispose();
+                return result;
+            }
+
+        }
+    }
+}
---
+++ SkyForgeConsoleTest/Services/FileSystem/FileSystemTest.cs
@@ -0,0 +1,65 @@
+﻿/**************************************************************************\
+    Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using NUnit.Framework;
+
+namespace SkyForgeConsoleTest.Services.FileSystem
+{
+    internal class FileSystemTest
+    {
+        [Test]
+        public void TestCreateDerectory()
+        {
+            var directoryName = "TestDirectory";
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+            }
+            SkyForgeConsole.Services.FileSystem.CreateDirectory(directoryName);
+            Assert.IsTrue(Directory.Exists(directoryName));
+            Directory.Delete(directoryName, true);
+        }
+
+        [Test]
+        public void TestIsHaveDirectory()
+        {
+            var directoryName = "TestDirectory";
+            Directory.CreateDirectory(directoryName);
+            Assert.IsTrue(SkyForgeConsole.Services.FileSystem.IsHaveDirectory(directoryName));
+            Directory.Delete(directoryName, true);
+        }
+
+        [Test]
+        public void TestIsHaveFile()
+        {
+            var fileName = "TestFile.txt";
+            if (!Path.Exists(fileName))
+            {
+                Thread thread = new Thread(_ => CreateFile(fileName));
+                thread.Start();
+                Thread.Sleep(100);
+            }
+            Assert.IsTrue(SkyForgeConsole.Services.FileSystem.IsHaveFile(fileName));         
+        }
+
+        [Test]
+        public void TestGetCurrentDirecotry()
+        {
+            Assert.IsTrue(Directory.GetCurrentDirectory().
+                          Equals(SkyForgeConsole.Services.FileSystem.GetCurrentDirectory()));
+        }
+
+        [Test]
+        public void TestGetFullPath()
+        {
+            var path = Directory.GetCurrentDirectory();
+            Assert.IsTrue(Path.GetFullPath(path).
+                          Equals(SkyForgeConsole.Services.FileSystem.GetFullPath(path)));    
+        }
+        private void CreateFile(string fileName)
+        {
+            File.Create(fileName);
+        }
+    }
+}
